dca4022bb0d753fbcfaba2146d51e58aef20d1fd
dongdk <dongdk.sysu@foxmail.com>
2015-11-24 19:52:29 +0800

    add exper for 3 banks of coarse model

---
+++ models/Pose/FLIC/ldp2/lecun-8x-3b-addc4_2-sd5_5-marks/draw_net.sh
@@ -0,0 +1,9 @@
+python_dir="../../../../../caffe/python/"
+cd $python_dir
+
+rankdir="TB"
+draw_net="draw_net.py"
+proto_file="../../models/Pose/FLIC/ldp2/lecun-8x-3b-addc4_2-sd5_5-marks/train_val.prototxt"
+image_file="../../models/Pose/FLIC/ldp2/lecun-8x-3b-addc4_2-sd5_5-marks/tb_train_val.jpg"
+
+python $draw_net $proto_file $image_file --rankdir=$rankdir

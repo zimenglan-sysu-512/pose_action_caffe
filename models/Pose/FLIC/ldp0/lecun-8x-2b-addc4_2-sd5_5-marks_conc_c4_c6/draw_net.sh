888736db2c67e0d00c6d640e64d2170f1dc82371
dongdk <dongdk.sysu@foxmail.com>
2015-12-28 09:55:59 +0800

    add exper conc_c4_c6

---
+++ models/Pose/FLIC/ldp0/lecun-8x-2b-addc4_2-sd5_5-marks_conc_c4_c6/draw_net.sh
@@ -0,0 +1,9 @@
+python_dir="../../../../../caffe/python/"
+cd $python_dir
+
+rankdir="TB"
+draw_net="draw_net.py"
+proto_file="../../models/Pose/FLIC/ldp0/lecun-8x-2b-addc4_2-sd5_5-marks_conc_c4_c6/train_val.prototxt"
+image_file="../../models/Pose/FLIC/ldp0/lecun-8x-2b-addc4_2-sd5_5-marks_conc_c4_c6/tb_train_val.jpg"
+
+python $draw_net $proto_file $image_file --rankdir=$rankdir

ca5e8122251f4b6a8759251aa216b802a641471a
dongdk <dongdk.sysu@foxmail.com>
2016-01-12 23:04:11 +0800

    add all convs exper -- replace pool with conv

---
+++ models/Pose/FLIC/black/lecun-8x-2b-sd5_5-marks-dsn-refine/draw_net.sh
@@ -0,0 +1,9 @@
+python_dir="../../../../../caffe/python/"
+cd $python_dir
+
+rankdir="TB"
+draw_net="draw_net.py"
+proto_file="../../models/Pose/FLIC/black/lecun-8x-2b-sd5_5-marks-dsn-refine/train_val.prototxt"
+image_file="../../models/Pose/FLIC/black/lecun-8x-2b-sd5_5-marks-dsn-refine/tb_train_val.jpg"
+
+python $draw_net $proto_file $image_file --rankdir=$rankdir

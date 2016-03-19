d479f5e530dab67108737b8d315367fa840d7c93
dongdk <dongdk.sysu@foxmail.com>
2016-03-19 20:23:58 +0800

    graduate - experiments - finetune fusnet

---
+++ models/Pose2/flic/d302/fusnet_tmask_2b_1_ft/viz_net.sh
@@ -0,0 +1,9 @@
+python_dir="../../../../../caffe/python/"
+cd $python_dir
+
+rankdir="TB"
+draw_net="draw_net.py"
+
+proto_file="../../models/Pose2/flic/d302/fusnet_tmask_2b_1_ft/train.pt"
+image_file="../../models/Pose2/flic/d302/fusnet_tmask_2b_1_ft/net.jpg"
+python $draw_net $proto_file $image_file --rankdir=$rankdir

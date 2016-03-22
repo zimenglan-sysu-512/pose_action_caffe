3cc185fe796777ea67234c5d075168ffc6c12162
dongdk <dongdk.sysu@foxmail.com>
2016-03-22 11:43:21 +0800

    graduate
    experiments - fusion net of kinect2

--- models/Pose2/flic/d302/refnet_fusnet_tmask_2b_1_ft_merge/run.sh
+++ models/Pose2/flic/d302/refnet_fusnet_tmask_2b_1_ft_merge/run.sh
@@ -8,36 +8,36 @@ caffe_dire="caffe/"
 tools_dire="build/tools/"
 tools_dire=$caffe_dire$tools_dire
 
-# models/Pose2/kinect2/d302/tmask_2b/
+# models/Pose2/flic/d302/refnet_fusnet_tmask_2b_1_ft_merge/
 pt_dire="models/Pose2/"
-sub_pt_dire="kinect2/d302/"
-exper_name="tmask_2b/"
+sub_pt_dire="flic/d302/"
+exper_name="refnet_fusnet_tmask_2b_1_ft_merge/"
 exper_path=$pt_dire$sub_pt_dire$exper_name
 mkdir -p $exper_path
 
-# models/Pose2/kinect2/d302/tmask_2b/solver.pt
+# models/Pose2/flic/d302/refnet_fusnet_tmask_2b_1_ft_merge/solver.pt
 solver_pt="solver.pt"
 solver_pt=$exper_path$solver_pt
 echo "\n" $solver_pt "\n"
 
-# ../asserts/models/Pose2/kinect2/d302/tmask_2b/
+# ../asserts/models/Pose2/flic/d302/refnet_fusnet_tmask_2b_1_ft_merge/
 model_dire="../asserts/"
 model_dire=$model_dire$exper_path
 mkdir -p $model_dire
 
-# ../asserts/models/Pose2/kinect2/d302/tmask_2b/models/
+# ../asserts/models/Pose2/flic/d302/refnet_fusnet_tmask_2b_1_ft_merge/models/
 model_path="models/"
 model_path=$model_dire$model_path
 mkdir -p $model_path
 echo $model_path
 
-# ../asserts/models/Pose2/kinect2/d302/tmask_2b/log/
+# ../asserts/models/Pose2/flic/d302/refnet_fusnet_tmask_2b_1_ft_merge/log/
 log_path="log/"
 log_path=$model_dire$log_path
 mkdir -p $log_path
 
 # prefix -- log file
-file_prefix="kinect2_"
+file_prefix="flic_"
 log_file=$(date -d "today" +"%Y-%m-%d-%H-%M-%S")
 log_file=$log_path$file_prefix$log_file".log"
 
@@ -52,24 +52,28 @@ echo "  sh run.sh [re_iter]"
 echo
 echo "######################################"
 echo
-sleep_time=1
-sleep $sleep_time
 
 # resume model file
 if [ ! -n "$1" ] ;then
 	re_iter=0
-	# run & log command
-	$caffe_bin train --solver=$solver_pt 2>&1 | tee -a $log_file
+	resume_model_file="flic_iter_"$re_iter".caffemodel"
 else
 	re_iter=$1
-	resume_model_file="kinect2_iter_"$re_iter".solverstate"
-	resume_model_file=$model_path$resume_model_file
-	echo
-	echo "re_iter:" $re_iter
-	echo "snapshot path:" $resume_model_file
-	echo
-	# run & log command
+	resume_model_file="flic_iter_"$re_iter".solverstate"
+fi
+resume_model_file=$model_path$resume_model_file
+echo
+echo "re_iter:" $re_iter
+echo "snapshot path:" $resume_model_file
+echo
+
+sleep_time=3
+sleep $sleep_time
+
+# run & log command
+if [ ! -n "$1" ] ;then
+	$caffe_bin train --solver=$solver_pt --weights=$resume_model_file 2>&1 | tee -a $log_file
+else
 	$caffe_bin train --solver=$solver_pt --snapshot=$resume_model_file 2>&1 | tee -a $log_file
 fi
-
 echo "Done!"

fa7cf704512ff3b010a655b4804d76fd0c5f90a8
dongdk <dongdk.sysu@foxmail.com>
2016-03-24 09:47:31 +0800

    flic exper -> refnet

---
+++ models/Pose2/flic/d302/refnet_fusnet_tmask_2b_2/viz_net.sh
@@ -0,0 +1,10 @@
+python_dir="../../../../../caffe/python/"
+cd $python_dir
+
+rankdir="TB"
+draw_net="draw_net.py"
+
+proto_file="../../models/Pose2/flic/d302/refnet_fusnet_tmask_2b_2/train.pt"
+image_file="../../models/Pose2/flic/d302/refnet_fusnet_tmask_2b_2/net.jpg"
+python $draw_net $proto_file $image_file --rankdir=$rankdir
+

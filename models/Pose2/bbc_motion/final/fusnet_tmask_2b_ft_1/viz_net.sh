a93d64182c8a7008e034026720be52aee3d16e61
dongdk <dongdk.sysu@foxmail.com>
2016-04-06 17:43:49 +0800

    bbc pose experi - modify only use 7 joints

---
+++ models/Pose2/bbc_motion/final/fusnet_tmask_2b_ft_1/viz_net.sh
@@ -0,0 +1,9 @@
+python_dir="../../../../../caffe/python/"
+cd $python_dir
+
+rankdir="TB"
+draw_net="draw_net.py"
+proto_file="../../models/Pose2/bbc_motion/final/fusnet_tmask_2b_ft_1/train.pt"
+image_file="../../models/Pose2/bbc_motion/final/fusnet_tmask_2b_ft_1/net.jpg"
+
+python $draw_net $proto_file $image_file --rankdir=$rankdir
